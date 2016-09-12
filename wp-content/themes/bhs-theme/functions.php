d20d224626a6a8065aa2c43dbda87017098e1da3
Vonetta <vonettastevenson@gmail.com>
2016-09-11 17:29:37 -0700

    sidebar and widgets

--- wp-content/themes/bhs-theme/functions.php
+++ wp-content/themes/bhs-theme/functions.php
@@ -38,4 +38,20 @@ function tutsplus_add_google_fonts() {
   wp_enqueue_style( 'googleFonts');
 }
 add_action( 'wp_enqueue_scripts', 'tutsplus_add_google_fonts' );
+
+function create_widget($name, $id, $description) {
+    register_sidebar(array(
+      'name' => __( $name ),
+      'id'   => $id,
+      'description' => __( $description ),
+      'before_widget' => '<div class="widget">',
+      'after_widget' => '</div>',
+      'before_title' => '<h2>',
+      'after_title' => '</h2>'
+    ));
+}
+
+create_widget( 'Front Page Left', 'front-left', 'Displays on the left of the hompage');
+create_widget( 'Front Page Center', 'front-center', 'Displays on the center of the hompage');
+create_widget( 'Front Page Right', 'front-right', 'Displays on the right of the hompage');
 ?>
